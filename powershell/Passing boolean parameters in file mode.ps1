33d699c597207768dedf1916804f76ce12d46535
mykalamos <40150157+mykalamos@users.noreply.github.com>
2018-06-13 19:50:45 +0100

    Update Passing boolean parameters in file mode.ps1

--- powershell/Passing boolean parameters in file mode.ps1
+++ powershell/Passing boolean parameters in file mode.ps1
@@ -1,2 +1,1 @@
-# use curly braces around arguments to make sure bool is evaluated correctly with File mode
-powershell -NonInteractive -ExecutionPolicy ByPass -File "MyPowershellScript.ps1" {-configuration Release -assembliesInParallel $True}
+# does not work. Use a string and the convert to boolean in the script
