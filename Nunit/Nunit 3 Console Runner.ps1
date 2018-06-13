55f6b4b2a9fb27af037572162cca27eb75761612
mykalamos <40150157+mykalamos@users.noreply.github.com>
2018-06-13 15:49:51 +0100

    Create Nunit 3 Console Runner.ps1

---
+++ Nunit/Nunit 3 Console Runner.ps1
@@ -0,0 +1,34 @@
+Param(
+	[string]$configuration="Debug",
+	[bool]$assembliesInParallel=$True
+	)
+
+$srcRoot="$PSScriptRoot\..\..\src"
+$targetAssemblyPrefix="MyApp.*Tests"
+
+Push-Location $srcRoot
+pwd
+
+if ($assembliesInParallel -eq $False)
+{
+	Write-Host "Assemblies will be run in series"
+	$nunitArgs = " --inprocess"
+}
+
+$cmdLine = "$srcRoot\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe " + `
+( `
+	ls -Recurse `
+	| Where-Object { `
+			$_.FullName -like "*\bin\$configuration\$targetAssemblyPrefix.Unit.dll" `
+		-or $_.FullName -like "*\bin\$configuration\$targetAssemblyPrefix.Integration.dll" } `
+	| sort-object -Unique Name `
+	| % FullName `
+) + $nunitArgs
+
+echo $cmdLine
+
+Invoke-Expression $cmdLine
+
+Pop-Location
+
+exit $LASTEXITCODE
