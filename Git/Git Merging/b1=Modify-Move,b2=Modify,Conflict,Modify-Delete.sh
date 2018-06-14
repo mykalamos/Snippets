2a76eba5a03ba8bde0dc35b6f9ba500723586c09
mykalamos <40150157+mykalamos@users.noreply.github.com>
2018-06-14 12:14:03 +0100

    Create b1=Modify-Move,b2=Modify,Conflict,Modify-Delete.sh

---
+++ Git/Git Merging/b1=Modify-Move,b2=Modify,Conflict,Modify-Delete.sh
@@ -0,0 +1,23 @@
+cd $TMP/GitMerging
+git checkout -b feature/b1
+printf "\nhello moon again" >> src/HelloMoon/hello-moon.txt 
+mkdir src/HelloMars
+mv src/HelloMoon/hello-moon.txt src/HelloMars/hello-moon-moved.txt
+
+git add . 
+git commit -m "create a 2nd line in hello-moon.txt and move it"
+
+git checkout master
+
+git checkout -b feature/b2
+printf "\nhello moon yet again" >> src/HelloMoon/hello-moon.txt 
+
+git add . 
+git commit -m "create a 2nd line in hello-moon.txt yet again"
+
+git checkout master
+git merge feature/b2
+git branch -d feature/b2
+
+git checkout feature/b1
+git merge master
