2ecb40acbba3dd029c4f38418a3691af21e9fccd
mykalamos <40150157+mykalamos@users.noreply.github.com>
2018-06-19 17:08:48 +0100

    Create b1=Modify,b2=Move,No Conflict.sh

---
+++ Git/Git Merging/b1=Modify,b2=Move,No Conflict.sh
@@ -0,0 +1,27 @@
+cd $TMP/GitMerging
+
+git checkout -b feature/b1
+
+printf "\nhello world again" >> src/HelloWorld/hello-world.txt
+
+git add .
+git commit -m "Add text to Context1.txt"
+
+git checkout master
+
+git checkout -b feature/b2
+
+mv src/HelloWorld/hello-world.txt src/HelloWorld/hello-worlda.txt
+git add .
+git commit -m "Rename file"
+
+git checkout master
+git merge feature/b1
+git branch -d feature/b1
+
+git checkout feature/b2
+git merge master
+
+git checkout master
+git merge feature/b2
+git branch -d feature/b2
