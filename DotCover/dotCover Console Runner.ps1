a163537ec5857e6c7e7f36784437f15d346f601a
mykalamos <40150157+mykalamos@users.noreply.github.com>
2018-06-29 10:54:42 +0100

    Create dotCover Console Runner.ps1

---
+++ DotCover/dotCover Console Runner.ps1
@@ -0,0 +1,37 @@
+Param([string]$configuration="Debug")
+
+$checkoutDir="$PSScriptRoot\..\.."
+$srcRoot="$checkoutDir\src"
+$nunitConsole="$checkoutDir\src\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe"
+$dotCoverRoot="$PSScriptRoot\..\..\..\..\tools\dotCover"
+$dotCoverOutputFile="$checkoutDir\dotCover_output.html"
+$filters="-:type=$appPrefix.*Tests.*;+:type=$appPrefix.*;"
+$targetAssemblyPrefix="MRS.*Tests"
+$attributeFilters="System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute"
+
+Push-Location $srcRoot
+pwd
+
+# N.B. We're ignoring the Integration Tests for now
+$testFileList = `
+( `
+	ls -Recurse `
+	| Where-Object { `
+			$_.FullName -like "*\bin\$configuration\$targetAssemblyPrefix.Unit.dll" `
+		-or $_.FullName -like "*\bin\$configuration\$targetAssemblyPrefix.Integration.dll" } `
+	| sort-object -Unique Name `
+	| % FullName `
+)
+
+Push-Location $dotCoverRoot
+pwd
+
+$cmdLine = "$dotCoverRoot\dotCover.exe analyse /TargetExecutable=`"$nunitConsole`" /TargetArguments=`"$testFileList`" /Output=`"$dotCoverOutputFile`" /ReportType=HTML /Filters=`"$filters`" /AttributeFilters=`"$attributeFilters`""
+
+echo $cmdLine
+
+Invoke-Expression $cmdLine
+
+Pop-Location
+
+exit $LASTEXITCODE
