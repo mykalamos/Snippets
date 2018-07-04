705bc7723471719e596fd3b78850bb998483e8a2
mykalamos <40150157+mykalamos@users.noreply.github.com>
2018-07-04 10:40:19 +0100

    Update dotCover Console Runner.ps1

--- DotCover/dotCover Console Runner.ps1
+++ DotCover/dotCover Console Runner.ps1
@@ -1,12 +1,13 @@
 Param([string]$configuration="Debug")
 
+$appPrefix="TODO"
 $checkoutDir="$PSScriptRoot\..\.."
 $srcRoot="$checkoutDir\src"
 $nunitConsole="$checkoutDir\src\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe"
 $dotCoverRoot="$PSScriptRoot\..\..\..\..\tools\dotCover"
 $dotCoverOutputFile="$checkoutDir\dotCover_output.html"
 $filters="-:type=$appPrefix.*Tests.*;+:type=$appPrefix.*;"
-$targetAssemblyPrefix="<Prefix>.*Tests"
+$targetAssemblyPrefix="$appPrefix.*Tests"
 $attributeFilters="System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute"
 
 Push-Location $srcRoot
