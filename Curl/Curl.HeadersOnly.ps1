b764846722cf8594fc36f38e3f620c5a9707091a
mykalamos <40150157+mykalamos@users.noreply.github.com>
2019-01-04 14:21:54 +0000

    Create Curl.HeadersOnly.ps1

---
+++ Curl/Curl.HeadersOnly.ps1
@@ -0,0 +1,15 @@
+Write-Output "*** Executing $($($MyInvocation.MyCommand.Name)) ***"
+
+[string] $curl = "$PSScriptRoot\..\..\lib\curl\curl-7.63.0-win64-mingw\bin\curl.exe";
+[string] $url = "http://server/app/api/v1.0/Controller/Method";
+[array] $headers = &$curl -X GET -I $url --retry 3 --retry-delay 2 -s
+
+foreach($header in $headers)
+{
+	Write-Host "Header=$header"
+	if($header -eq 'HTTP/1.1 200 OK')
+	{
+		return 0;
+	}
+}
+return 1;
