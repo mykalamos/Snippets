31d1637dc83821be0ddc4ef62d0491026ee62d47
mykalamos <40150157+mykalamos@users.noreply.github.com>
2019-01-04 17:24:39 +0000

    Create Restore.ps1

---
+++ dotnet/Restore.ps1
@@ -0,0 +1,8 @@
+Param(
+  [string] $Version=0.0.0
+)
+Write-Output "*** Executing $($MyInvocation.MyCommand.Name) ***"
+& dotnet restore "MySolution.sln" --source $pathToPackages /p:Version=$Version
+exit $LastExitCode
+
+# Not sure if version is required
