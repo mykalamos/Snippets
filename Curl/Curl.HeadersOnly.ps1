901d0f1d80f1696b8cb456cc6c4bc2342a2bea1f
mykalamos <40150157+mykalamos@users.noreply.github.com>
2019-01-10 09:59:27 +0000

    Update Curl.HeadersOnly.ps1

--- Curl/Curl.HeadersOnly.ps1
+++ Curl/Curl.HeadersOnly.ps1
@@ -1,8 +1,8 @@
 Write-Output "*** Executing $($($MyInvocation.MyCommand.Name)) ***"
 
-[string] $curl = "$PSScriptRoot\..\..\lib\curl\curl-7.63.0-win64-mingw\bin\curl.exe";
+[string] $curlPath = "curl\curl-7.63.0-win64-mingw\bin\curl.exe";
 [string] $url = "http://server/app/api/v1.0/Controller/Method";
-[array] $headers = &$curl -X GET -I $url --retry 3 --retry-delay 2 -s
+[array] $headers = &$curlPath -X GET -I $url --retry 3 --retry-delay 2 -s
 
 foreach($header in $headers)
 {
