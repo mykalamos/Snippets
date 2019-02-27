ec2d26dea8fc55a22dbf4a1d9452d033e3ddcab5
mykalamos <40150157+mykalamos@users.noreply.github.com>
2019-02-27 12:51:16 +0000

    Update Row_Number.sql

--- Sql/Row_Number.sql
+++ Sql/Row_Number.sql
@@ -1,11 +1,11 @@
-DECLARE @myInts TABLE(IntValue INT, ParitionId INT);
+DECLARE @myInts TABLE(IntValue INT, PartitionId INT);
 INSERT INTO @myInts VALUES (27,2),(3,1),(4,1),(NULL,2);
 
 SELECT
 	 [t].[IntValue]
 	,ROW_NUMBER()
 		OVER (
-			PARTITION BY [t].[ParitionId] -- Optional
+			PARTITION BY [t].[PartitionId] -- Optional
 			ORDER BY [t].[IntValue] -- Mandatory - does not support constants
 			) AS [Id]
 FROM
