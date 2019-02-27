f423f700add94206a942e8356b72f4a52cb7a64e
mykalamos <40150157+mykalamos@users.noreply.github.com>
2019-02-27 12:50:39 +0000

    Update Row_Number.sql

--- Sql/Row_Number.sql
+++ Sql/Row_Number.sql
@@ -1,11 +1,12 @@
-DECLARE @myInts IntTableType;
-INSERT INTO @myInts VALUES (27),(3),(4),(NULL);
+DECLARE @myInts TABLE(IntValue INT, ParitionId INT);
+INSERT INTO @myInts VALUES (27,2),(3,1),(4,1),(NULL,2);
 
 SELECT
-		[t].[IntValue]
-		,ROW_NUMBER()
-			OVER (
-				ORDER BY NULL
-				) AS Id
+	 [t].[IntValue]
+	,ROW_NUMBER()
+		OVER (
+			PARTITION BY [t].[ParitionId] -- Optional
+			ORDER BY [t].[IntValue] -- Mandatory - does not support constants
+			) AS [Id]
 FROM
 	@myInts t;
