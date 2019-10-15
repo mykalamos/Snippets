3af6e0f4b418232ddac311588cb897fba4b04ed9
mykalamos <40150157+mykalamos@users.noreply.github.com>
2019-10-15 18:45:23 +0100

    Update Xml.Poke.ps1

--- powershell/Xml.Poke.ps1
+++ powershell/Xml.Poke.ps1
@@ -1,5 +1,5 @@
 [string] $configPath = 'c:\temp\AppSettings.config';
 [xml] $xml = Get-Content $configPath
-$node = $xml.SelectSingleNode('//appSettings/add[@key="TimeoutValue"]')
+$node = $xml.SelectSingleNode('/appSettings/add[@key="TimeoutValue"]')
 $node.Value = '10:00:00';
 $xml.Save($configPath);
