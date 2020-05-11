db336f002a8727bf81247d4224a043d17fbbff08
Riky Choudhuri <kalamos.github@outlook.com>
2020-05-11 18:37:04 +0100

    Initial commit

---
+++ Git/Generate.bash.commit.and.push.ps1
@@ -0,0 +1,9 @@
+﻿cls
+gci -Directory C:\users\riky\documents\code\News\packages | select -ExpandProperty fullName | %{ $s = $_ -replace ':',''; $s = $s -replace '\\','/'; "/$s" }
+
+# Nimbletext
+
+#cd $0
+#git add .
+#git ci -m "Package commit"
+#git push
