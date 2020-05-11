6569fa20fe4a5c9d59e5a9d9f634d100bbc157f7
Riky Choudhuri <kalamos.github@outlook.com>
2020-05-11 18:44:23 +0100

    Remove nimbletext. generate bash in ps

--- Git/Generate.bash.commit.and.push.ps1
+++ Git/Generate.bash.commit.and.push.ps1
@@ -1,9 +1,10 @@
 ﻿cls
-gci -Directory C:\users\riky\documents\code\News\packages | select -ExpandProperty fullName | %{ $s = $_ -replace ':',''; $s = $s -replace '\\','/'; "/$s" }
-
-# Nimbletext
-
-#cd $0
-#git add .
-#git ci -m "Package commit"
-#git push
+gci -Directory C:\users\riky\documents\code\News\packages | select -ExpandProperty fullName | 
+%{ 
+    $s = $_ -replace ':',''; 
+    $s = $s -replace '\\','/';
+    "cd /$s" ;
+    "git add ."
+    "git ci -m `"Package commit`""
+    "git push"
+}
